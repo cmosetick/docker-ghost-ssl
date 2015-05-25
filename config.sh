efe67a4d24802c3f42f2df5011564a9bd4e861d9
Chris Mosetick <cmosetick@gmail.com>
2015-05-25 22:04:40 +0000

    add contanier auto_restart_policy options to config.sh

--- config.sh
+++ config.sh
@@ -13,3 +13,14 @@ ssl_crt="${cert_folder}/server.crt"
 ssl_key="${cert_folder}/server.key"
 nginx_sites_folder="${data_folder}/sites-enabled"
 nginx_ghost_conf="${nginx_sites_folder}/ghost.conf"
+
+#define restart policy for containers
+
+#leave set to always for production use
+#this will make sure that your containers get started back up following a host reboot
+auto_restart_policy="--restart='always'"
+
+#set this to no for testing or development
+#auto_restart_policy="--restart='no'"
+
+#Ref: https://docs.docker.com/reference/run/#restart-policies-restart
